a5f7f9653db593664110dff7c0c7ef8d656967f4
Rinde van Lon <rindevanlon@gmail.com>
2016-09-01 15:02:06 +0200

    back to evo reauct

--- src/main/java/com/github/rinde/gpem17/GPEM17.java
+++ src/main/java/com/github/rinde/gpem17/GPEM17.java
@@ -116,8 +116,8 @@ public class GPEM17 {
     StochasticSupplier<? extends Communicator> cm =
       EvoBidder.realtimeBuilder(solver, OBJ_FUNC)
         .withReauctionCooldownPeriod(60000)
-        .withCheapestInsertionHeuristicForReauction();
-    // .withPriorityHeuristicForReauction();
+        // .withCheapestInsertionHeuristicForReauction();
+        .withPriorityHeuristicForReauction();
 
     String name = "ReAuction-RP-EVO-BID-EVO-" + id;
     return GPEM17.createConfig(solver, rp, cm, true, name);
@@ -133,8 +133,8 @@ public class GPEM17 {
     StochasticSupplier<? extends Communicator> cm =
       EvoBidder.simulatedTimeBuilder(solver, OBJ_FUNC)
         .withReauctionCooldownPeriod(60000)
-        .withCheapestInsertionHeuristicForReauction();
-    // .withPriorityHeuristicForReauction();
+        // .withCheapestInsertionHeuristicForReauction();
+        .withPriorityHeuristicForReauction();
 
     String name = "ReAuction-RP-EVO-BID-EVO-" + id;
 
