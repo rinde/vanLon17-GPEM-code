6ebefb82a6a2dcd939000ce2986dc29e700d6806
Rinde van Lon <rindevanlon@gmail.com>
2016-10-13 17:33:40 +0200

    added script for time measure experiment

---
+++ time-measure-exp.sh
@@ -0,0 +1,1 @@
+nohup mvn clean compile -U exec:java -Dexec.mainClass="com.github.rinde.gpem17.eval.Evaluate" -Dexec.args="realtime tmp.txt EVO 1-1-1 OptaPlanner EnableTimeMeasurements -repetitions 1  -sf regex:.*0\.50-20-10\.00-0\.scen" &
