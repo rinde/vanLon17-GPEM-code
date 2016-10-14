4577c0d62a6fe4c4f3e055f08b332ebb0099da02
Rinde van Lon <rindevanlon@gmail.com>
2016-10-14 14:32:53 +0200

    added script for delayed-heuristic-experiment

---
+++ delayed-heuristic-exp.sh
@@ -0,0 +1,2 @@
+nohup mvn clean compile -U exec:java -Dexec.mainClass="com.github.rinde.gpem17.eval.Evaluate" -Dexec.args="realtime tmp.txt EVO 1-1-1 OptaPlanner EnableTimeMeasurements heuristic-comp-delay:50ms run-optaplanner-mas:false -repetitions 1 -g false -sf regex:.*0\.50-20-10\.00.*\.scen" &
+
