85d0f8e945472738b755a95a3eaa419c906f2315
Rinde van Lon <rindevanlon@gmail.com>
2017-01-17 11:13:13 +0100

    formatted shell scripts

--- delayed-heuristic-exp.sh
+++ delayed-heuristic-exp.sh
@@ -1,2 +1,13 @@
-nohup mvn clean compile -U exec:java -Dexec.mainClass="com.github.rinde.gpem17.eval.Evaluate" -Dexec.args="realtime tmp.txt EVO 1-1-1 OptaPlanner EnableTimeMeasurements heuristic-comp-delay:100ms run-optaplanner-mas:false -repetitions 1 -g false -sf regex:.*0\.50-20-10\.00.*\.scen" &
+nohup mvn clean compile -U exec:java -Dexec.mainClass="com.github.rinde.gpem17.eval.Evaluate" \
+-Dexec.args="realtime \
+tmp.txt \
+EVO \
+1-1-1 \
+OptaPlanner \
+EnableTimeMeasurements \
+heuristic-comp-delay:100ms \
+run-optaplanner-mas:false \
+-repetitions 1 \
+-g false \
+-sf regex:.*0\.50-20-10\.00.*\.scen" &
 
